769618b96c054814f5e414f70b6d4c39aac6df94
not_known200 <unk.aminjon@gmail.com>
2026-03-16 23:44:08 +0500

    Add dotenv support for bot token configuration
    Load environment variables and set up bot token.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
+from dotenv import load_dotenv
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from config import TOKEN
 from pyzbar.pyzbar import decode
 from PIL import Image
 import tempfile
@@ -11,7 +11,8 @@ logging.basicConfig(
     level=logging.INFO,  # Less verbose
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
-
+load_dotenv()
+TOKEN=os.getenv("BOT_TOKEN")
 bot = Bot(token=TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
